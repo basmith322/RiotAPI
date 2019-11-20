e1bee6eab681cdf0270b450018d5da66b42b0704
Benjamin Smith <smith-b9@ulster.ac.uk>
2019-11-20 15:05:07 +0000

    Implemented menu activity and blank functions for each option

--- app/src/main/java/com/example/riotapi/MenuActivity.kt
+++ app/src/main/java/com/example/riotapi/MenuActivity.kt
@@ -1,12 +1,39 @@
 package com.example.riotapi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+
+    fun goToLookup(view: View) {
+        val intent = Intent(this, LookupSummonerActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToTop10(view: View) {
+        //TODO
+    }
+
+    fun goToServerStatus(view: View)  {
+        //TODO
+    }
+
+    fun goToMatchHistory(view: View) {
+        //TODO
+    }
+
+    fun goToChampRotation(view: View) {
+        //TODO
+    }
+
+    fun goToSummonerProfile(view: View) {
+        //TODO
     }
 }
