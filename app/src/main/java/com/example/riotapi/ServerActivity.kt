2710442ff321fd27f75f64043b259ba7a5ea992c
Benjamin Smith <smith-b9@ulster.ac.uk>
2019-11-25 12:11:32 +0000

    Implimented spinner and server status selection

--- app/src/main/java/com/example/riotapi/ServerActivity.kt
+++ app/src/main/java/com/example/riotapi/ServerActivity.kt
@@ -3,6 +3,11 @@ package com.example.riotapi
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.StrictMode
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.*
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.merakianalytics.orianna.Orianna
 import com.merakianalytics.orianna.types.common.Region
@@ -21,7 +26,7 @@ class ServerActivity : AppCompatActivity() {
         Orianna.setRiotAPIKey(getString(R.string.RiotAPiKey))
         Orianna.setDefaultRegion(Region.EUROPE_WEST)
 
-        /*val regions = resources.getStringArray(R.array.spnRegion)
+        val regions = resources.getStringArray(R.array.spnRegion)
 
         val spinner = findViewById<Spinner>(R.id.spinnerRegion)
 
@@ -31,35 +36,42 @@ class ServerActivity : AppCompatActivity() {
             spinner.adapter = adapter
 
             spinner.onItemSelectedListener = object :
-                    AdapterView.OnItemSelectedListener {
+                    OnItemSelectedListener {
                 override fun onNothingSelected(p0: AdapterView<*>?) {
                     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
                 }
+
                 override fun onItemSelected(parent: AdapterView<*>,
                                             view: View, position: Int, id: Long) {
                     when (position) {
-                        1 -> Orianna.setDefaultRegion(Region.BRAZIL)
-                        2 -> Orianna.setDefaultRegion(Region.EUROPE_NORTH_EAST)
-                        3 -> Orianna.setDefaultRegion(Region.EUROPE_WEST)
-                        4 -> Orianna.setDefaultRegion(Region.JAPAN)
-                        5 -> Orianna.setDefaultRegion(Region.KOREA)
-                        6 -> Orianna.setDefaultRegion(Region.LATIN_AMERICA_NORTH)
-                        7 -> Orianna.setDefaultRegion(Region.LATIN_AMERICA_SOUTH)
-                        8 -> Orianna.setDefaultRegion(Region.NORTH_AMERICA)
-                        9 -> Orianna.setDefaultRegion(Region.OCEANIA)
-                        10 -> Orianna.setDefaultRegion(Region.RUSSIA)
-                        11 -> Orianna.setDefaultRegion(Region.TURKEY)
+                        0 -> Orianna.setDefaultRegion(Region.BRAZIL)
+                        1 -> Orianna.setDefaultRegion(Region.EUROPE_NORTH_EAST)
+                        2 -> Orianna.setDefaultRegion(Region.EUROPE_WEST)
+                        3 -> Orianna.setDefaultRegion(Region.JAPAN)
+                        4 -> Orianna.setDefaultRegion(Region.KOREA)
+                        5 -> Orianna.setDefaultRegion(Region.LATIN_AMERICA_NORTH)
+                        6 -> Orianna.setDefaultRegion(Region.LATIN_AMERICA_SOUTH)
+                        7 -> Orianna.setDefaultRegion(Region.NORTH_AMERICA)
+                        8 -> Orianna.setDefaultRegion(Region.OCEANIA)
+                        9 -> Orianna.setDefaultRegion(Region.RUSSIA)
+                        10 -> Orianna.setDefaultRegion(Region.TURKEY)
                         else -> {
                             Orianna.setDefaultRegion(Region.EUROPE_WEST)
                         }
                     }
+                    val server = Orianna.getShardStatus()
+                    textViewRegion.text = """${getString(R.string.txtRegion)}${server.regionTag}"""
+                    textViewGameServerStatus.text = """${getString(R.string.txtServerStatus)} 
+                        |${server.region.status.services[0].status.toUpperCase()}"""
+                            .trimMargin()
+                    textViewClientServerStatus.text = """${getString(R.string.txtClientStatus)} 
+                        |${server.region.status.services[3].status.toUpperCase()}""".trimMargin()
+                    textViewWebsiteServerStatus.text = """${getString(R.string.txtWebsiteStatus)} 
+                        |${server.region.status.services[2].status.toUpperCase()}""".trimMargin()
+
                 }
             }
-        }*/
-
-        val serverStatus = Orianna.shardStatusWithRegion(Region.EUROPE_WEST)
-        textViewServerStatus.text = serverStatus.get().region.status.services[0].status.toString().toUpperCase()
+        }
     }
 }
-
 
